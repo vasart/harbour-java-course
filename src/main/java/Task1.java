db6af6cba471fee0b804a7ca52815e4dc5725144
Vasilii Artemev <vasart@gmail.com>
2018-11-09 11:35:44 +0100

    Adedd gender detector with some tests

--- src/main/java/Task1.java
+++
@@ -1,9 +0,0 @@
-public class Task1 {
-        
-    public int plus(int a, int b) {
-        return a + b;
-    }
-    
-    public static void Main(String[] args) {
-    }
-}
--- src/test/java/Task1Test.java
+++
@@ -1,8 +0,0 @@
-import junit.framework.TestCase;
-
-public class Task1Test extends TestCase {
-
-    public void testTwoPlusTwo() {
-        assertEquals(4, new Task1().plus(2, 2));
-    }
-}
