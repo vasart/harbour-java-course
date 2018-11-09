358f89aa9d676e506bee4792b6f44ea423582e74
Vasilii Artemev <vasart@gmail.com>
2018-11-09 01:40:04 +0100

    Changed test name

--- src/test/java/Task1Test.java
+++ src/test/java/Task1Test.java
@@ -1,15 +1,8 @@
 import junit.framework.TestCase;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class Task1Test extends TestCase {
 
-    public Task1Test() {
-    }
-
-    @Test
-    void twoPlusTwo() {
-        assertEquals(4, new Task1().plus(2,2));
+    public void testTwoPlusTwo() {
+        assertEquals(4, new Task1().plus(2, 2));
     }
 }
